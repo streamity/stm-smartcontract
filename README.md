8a40ef8df1307a4599d15965dde14bb28aae42cb
Pavel Bolgar <gfijrb@yandex.ua>
2017-12-03 21:54:59 +0200

    dev

---
+++ README.md
@@ -0,0 +1,5 @@
+# Streamity Smart Contract
+
+``` 
+Streamity smart contract based on ERC20 tokens, build on truffle framework. 
+```
--- contracts/Ownable.sol
+++ contracts/Ownable.sol
@@ -1,5 +1,10 @@
 pragma solidity ^0.4.18;
 
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
 contract Ownable
 {
     address public owner;
